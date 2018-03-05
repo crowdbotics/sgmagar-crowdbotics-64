23fa8e57fd449590713e9252953788b4a60e43ab
Saurav <sp.gharti@gmail.com>
2018-03-05 20:09:09 +0000

    add package django-twitter

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-twitter', 'url': 'http://pypi.python.org/pypi/django-twitter/0.1.0'},
     ]
     context = {
         'title': 'sgmagar-crowdbotics-64',
